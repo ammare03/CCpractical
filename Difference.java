d5467a8b2dfb7a8c061a736fbb90979523e725ec
Ammar <engineer.ammar18724@gmail.com>
2024-01-18 10:02:46 +0530

    Committing new file

---
+++ Difference.java
@@ -0,0 +1,9 @@
+public class Difference {
+    public static void main(String args[]) {
+        int a = 25;
+        int b = 6;
+        int c = 8;
+        int d = a - b - c;
+        System.out.println(d);
+    }
+}
