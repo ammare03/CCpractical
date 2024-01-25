83c47d75c0b051ee5ccaf16576182a6be243c904
Ammar <engineer.ammar18724@gmail.com>
2024-01-25 09:26:37 +0530

    Multiply committed

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 6;
+        int c = 8;
+        int d = a * b * c;
+        System.out.println(d);
+    }
+}
